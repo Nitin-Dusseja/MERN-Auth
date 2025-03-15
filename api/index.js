b93a572fbcd460418e29c9aa08266581218da961
Nitin Dusseja <151530915+Nitin-Dusseja@users.noreply.github.com>
2025-03-16 00:24:51 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: 'https://mern-auth-pyww.onrender.com',
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
