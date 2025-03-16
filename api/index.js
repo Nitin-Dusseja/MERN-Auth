333dc21ceb73d14ada38a1a192a9c41d5dc8e5a2
Nitin Dusseja <151530915+Nitin-Dusseja@users.noreply.github.com>
2025-03-16 23:16:01 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,6 +14,7 @@ const port = 4000;
 connectDB();
 
 app.use('/api', require('./routes/api/auth')); 
+app.use('/api', require('./routes/api/user')); 
 
 app.use(express.static(path.join(__dirname, "/client/dist")));
 
